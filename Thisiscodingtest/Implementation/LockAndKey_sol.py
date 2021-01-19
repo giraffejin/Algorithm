003f66ada5071cd46fb93aa8e8605e81b9994265
giraffejin <hjpearl1004@naver.com>
2021-01-19 09:44:03 +0900

    기출Q10 - 자물쇠와 열쇠 답안 해설

--- Thisiscodingtest/Implementation/LockAndKey_sol.py
+++ Thisiscodingtest/Implementation/LockAndKey_sol.py
@@ -1,10 +1,10 @@
-#구현 기출 문제 - 자물쇠와 열쇠 (chapter12-10)
+#구현 기출 문제 - 자물쇠와 열쇠 (chapter12-10) 답안 해설
 
 #자물쇠의 크기와 열쇠의 크기가 20x20보다 작기때문에 완전 탐색을 이용하여 열쇠를 이동이나 회전시켜서 자물쇠에 끼워보는 작업을 전부 시도
 #완전 탐색을 수월하게 하기 위해 자물쇠 리스트의 크기를 3배 이상으로 변경하여 활용함
 #새로 만든 자물쇠 리스트의 정중앙에 기존의 자물쇠를 위치한다
 
-#90도 회전하는 함수
+#2차원 리스트 90도 회전하는 함수
 def rotate(a):
     n = len(a)
     m = len(a[0])
@@ -15,6 +15,7 @@ def rotate(a):
     return result
 
 #자물쇠가 풀리는지 확인하는 함수(자물쇠에 해당하는 부분이 모두 1로 채워져 있는지 확인)
+#자물쇠의 중간 부분이 모두 1인지 확인
 def check(new_lock):
     lock_length = len(new_lock)//3
     for i in range(lock_length,2*lock_length):
@@ -27,7 +28,7 @@ def check(new_lock):
 def solution(key,lock):
     n = len(lock)
     m = len(key)
-    #세 배짜리 자물쇠를 만들기
+    #자물쇠의 크기를 기존 3배로 변환
     new_lock = [[0]*(3*n) for _ in range(3*n)]
     #확대한 자물쇠의 중앙에 원래 자물쇠를 위치하기
     for i in range(n):
@@ -35,27 +36,25 @@ def solution(key,lock):
             new_lock[n+i][n+j] = lock[i][j]
     
     #회전이 가능한 4가지의 방향에 대해 확인
-    for _ in range(4):
+    for rotation in range(4):
         #key를 회전
         rotate(key)
         for i in range(2*n):
             for j in range(2*n):
-                #자물쇠에 열쇠를 넣어 값 구하기
+                #자물쇠에 열쇠를 넣어 값 구하기(자물쇠에 열쇠를 끼워 넣기)
                 for a in range(m):
                     for b in range(m):
                         new_lock[i+a][j+b] += key[a][b]
 
-                #자물쇠의 합이 1인지 확인
+                #자물쇠의 합이 1인지 확인(새로운 자물쇠에 열쇠가 정확히 들어맞는지 검사)
                 if check(new_lock)==True:
-                    answer = 'true'
-                    return answer
+                    return True
 
                 #자물쇠를 다시 빼기
                 for a in range(m):
                     for b in range(m):
                         new_lock[i+a][j+b]-=key[a][b]
-    answer = 'false'
-    return answer
+    return False
 
 key = [[0,0,0],[1,0,0],[0,1,1]]
 lock = [[1,1,1],[1,1,0],[1,0,1]]
